990f5c1c814b5f2badd36c76a1491477d8d06928
Paolo Losi <paolo.losi@gmail.com>
2011-01-25 22:48:52 +0100

    setup.py uses distutils

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup, find_packages, Extension
+import distutils.core
 
-setup(
-    name = 'stormed',
-    version = '0.1',
-    packages = find_packages(exclude=['test*']),
-    include_package_data = True,
+try:
+    # to enable "python setup.py develop"
+    import setuptools
+except ImportError:
+    pass
+
+distutils.core.setup(
+    name="stormed-amqp",
+    version='0.1',
+    packages = ["stormed", "stormed.method", "stormed.method.codegen"],
+    author="Paolo Losi",
+    author_email="paolo.losi@gmail.com",
+    download_url="https://github.com/paolo-losi/stormed-amqp/stormed-amqp-0.1.tar.gz",
+    license="http://www.opensource.org/licenses/mit-license.html",
+    description="native tornadoweb amqp 0-9-1 client implementation",
 )
-
